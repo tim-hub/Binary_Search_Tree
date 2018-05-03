da2593af1b5c78eb91a0e32280d47902051d338a
tim-hub <tim_w@outlook.com>
2018-05-03 16:53:43 +1200

    init and add add node and add node recursively functions

---
+++ Tree.c
@@ -0,0 +1,86 @@
+/*
+
+
+Platform:
+- GCC Version: (Apple LLVM version 9.0.0 (clang-900.0.39.2) Target: x86_64-apple-darwin16.7.0)
+- Online Complier (https://www.onlinegdb.com/online_c_compiler)
+Author: Tim
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+  int     data;
+  struct node* left;
+  struct node* right;
+}Node;
+
+typedef struct{
+  Node*    root;
+}BSTHead;
+
+/*
+  Create The Binary Search Tree
+*/
+BSTHead* CreateBST(){
+  BSTHead* myTree =  (BSTHead *) malloc (sizeof(BSTHead));
+  myTree -> root = NULL;
+  return myTree;
+}
+
+void AddNodeRecursively(Node* root, int data){
+  if (root -> data >= data){
+    if (root -> right == NULL){
+      Node* pNew = (Node *) malloc(sizeof(Node));
+      pNew -> data = data;
+      pNew -> left = NULL;
+      pNew -> right = NULL;
+
+      root -> right = pNew;
+    }else {
+      AddNodeRecursively(root -> right, data);
+    }
+  }else{
+    if (root -> left == NULL){
+      Node* pNew = (Node *) malloc(sizeof(Node));
+      pNew -> data = data;
+      pNew -> left = NULL;
+      pNew -> right = NULL;
+
+      root -> left = pNew;
+    }else {
+      AddNodeRecursively(root -> left, data);
+    }
+  }
+}
+
+void AddNode(BSTHead* myBST, int data){
+
+  if(myBST-> root ==NULL){
+
+    Node* pNew = (Node *) malloc(sizeof(Node));
+    pNew -> left = NULL;
+    pNew -> right = NULL;
+    pNew -> data = data;
+
+    myBST ->root = pNew;
+
+  }else{
+    // Node* root =(Node *) malloc(sizeof(Node));
+    // root =myBST -> root;
+    AddNodeRecursively(myBST -> root, data);
+  }
+
+}
+
+int main(){
+  BSTHead* myBST = CreateBST();
+
+  AddNode(myBST, 7);
+  AddNode(myBST, 7);
+
+  // system("pause");
+  return 0;
+
+}
